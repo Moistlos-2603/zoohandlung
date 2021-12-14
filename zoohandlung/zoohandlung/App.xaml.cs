7ab9ecb28ec198f61885e8803b17a7c071d0e838
18Markus1984 <markusschmidt1984@gmx.de>
2021-12-14 11:57:47 +0100

    Setup

--- zoohandlung/zoohandlung/App.xaml.cs
+++ zoohandlung/zoohandlung/App.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows;
 namespace zoohandlung
 {
     /// <summary>
-    /// Interaktionslogik für "App.xaml"
+    /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
