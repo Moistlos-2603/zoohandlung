1aecd513c012dd09489704c9c6c82097dc16c38c
18Markus1984 <markusschmidt1984@gmx.de>
2021-12-14 12:40:52 +0100

    Klassen anlegen, und teilweise variablen

---
+++ zoohandlung/zoohandlung/Tier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zoohandlung
+{
+    class Tier : Produkte
+    {
+        public string rasse;
+        public string name;
+        public bool saeugetier;
+        public string geschlecht;
+        public Tier(string bezeichnung) : base(bezeichnung)
+        {
+
+        }
+    }
+}
