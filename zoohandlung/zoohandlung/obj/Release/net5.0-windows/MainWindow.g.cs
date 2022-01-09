fbae1afb77b09a86ec1ada0b4840e0f81e2bfac5
moritz.albach@t-online.de <moritz.albach@t-online.de>
2022-01-09 15:51:09 +0100

    idk

--- zoohandlung/zoohandlung/obj/Release/net5.0-windows/MainWindow.g.cs
+++ zoohandlung/zoohandlung/obj/Release/net5.0-windows/MainWindow.g.cs
@@ -53,7 +53,7 @@ namespace zoohandlung {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/zoohandlung;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/zoohandlung;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
